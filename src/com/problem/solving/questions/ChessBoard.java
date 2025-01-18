9135d16fcaa2398b7704db417d3f81e4e522abf1
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2025-01-18 12:02:10 +0530

    Uploading Project

---
+++ src/com/problem/solving/questions/ChessBoard.java
@@ -0,0 +1,58 @@
+package com.problem.solving.questions;
+
+import java.util.Scanner;
+
+class ChessBoard {
+
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+		 * class should be named Solution.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a String...");
+
+		String s = sc.next();
+
+		String arr[][] = new String[][] {
+
+				{ "Black", "White", "Black", "White", "Black", "White", "Black", "White" },
+				{ "White", "Black", "White", " Black", "White", "Black", "White", "Black" },
+				{ "Black", "White", "Black", "White", "Black", "White", "Black", "White" },
+				{ "White", "Black", "White", " Black", "White", "Black", "White", "Black" },
+				{ "Black", "White", "Black", "White", "Black", "White", "Black", "White" },
+				{ "White", "Black", "White", " Black", "White", "Black", "White", "Black" },
+				{ "Black", "White", "Black", "White", "Black", "White", "Black", "White" },
+				{ "White", "Black", "White", " Black", "White", "Black", "White", "Black" },
+
+		};
+
+		String str = " ";
+
+		System.out.println(arr.length);
+
+		for (int i = 1, ch = 'a'; i < arr.length; i++, ch++) {
+
+			char c = (char) ch;
+			System.out.println(c);
+
+			for (int j = 1; j < arr[i].length; j++) {
+				System.out.println(j);
+
+				String copy = String.valueOf(c) + j;
+
+				if (copy.equals(s)) {
+					str = arr[i][j];
+					break;
+
+				}
+
+			}
+		}
+
+		System.out.print(str);
+
+	}
+}
